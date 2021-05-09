a327c8fd0992483443f54862bf3b017b5a1f1865
dilnuras <77263709+dilnuras@users.noreply.github.com>
2021-05-09 15:24:05 +0600

    hw36

--- JS/urok36/script.js
+++ JS/urok36/script.js
@@ -1,91 +1,60 @@
 "use strict";
       //Использование строгог режима для обработки данных
+    
+      //ООП понятие
+      //Применеие
+      //
+
+      //Как понять что есть строка
+
+
+      let car = {
+          kuzov: "jelezo",
+          kolesa: "4 koleca",
+          modal: function(){
+              console.log("Hello");
+          }
+      };
+
+    //   let mb = {
+    //       marka: "mers",
+    //       god: 1999
+    //   };
+
+    let mb =Object.create(car);
+    // mb.__proto__ = car;
+
+     //Object.setPrototypeOf(mb,car);
       
-      //Передача по ссылке или по значению
+
+    //  console.log(mb.modal);
+
+    //  mb.modal();
+
+
+
+    //Homework
+
+    let salary = {
+        almaz: 1200,
+        adilet: 1300,
+        nursultan: 3000
        
-          //Копирование обЪекта
-          //Копирование массива
-          
-          //Новые стандарты
-          //Оператор разворота Spread
+    };
+    
+    let bonus = {
+        almaz: 400,
+        adilet: 300,
+        nursultan: 200,
+    };
+    
+   let obj ={};
+Object.keys(salary ).forEach(function(a){
+  obj[a] = salary[a] +bonus[a];
 
-const arr1= {
-  a: 1,
-  b:2
-};
-//console.log//(arr1);
-const newArr = arr1;//ссылка на сам объект
-//console.log(newArr);
+});
+console.log(obj);
+    
+    
 
-newArr.a = 10;
-
-//console.log(newArr);
-//console.log(arr1);
-
-function copy_arr(myObj){
-  const newObj = {};
-
-  let key;
-  for(key in myObj ){
-      newObj [key] = myObj [key];
-  }
-  return newObj;
-}
-
-
-const oldObj = {
-    name: "Dilnura",
-    surname: "Seydalieva",
-    hobby: {
-        1:"cooking",
-        2:"running"
-    }
-};
-//console.log(oldObj);
-const newObj = copy_arr(oldObj);
-
-newObj.name = "Ami";
-// console.log(newObj.name);
-// console.log(oldObj.name);
-
-newObj.hobby[1] = "Reading";
-
-// console.log(newObj);
-// console.log(oldObj);
-
-const newAssign = Object.assign({},oldObj);
-
-newAssign.name = "Panda";
-// console.log(newAssign);
-// console.log(oldObj);
-
-const arr = ["apple","lemon","banana"];
-//индексы       0       1       2
-
-// const arr2 = arr.slice();
-
-// arr2[2] = "cherry";
-// console.log(arr);
-// console.log(arr2);
-
-const tehnika = ["phone","ipad","computer"],
-      music = ["rock","pop","jazz"],
-      combo = [...tehnika, ...music, "swimming","jump"];
-
-//console.log(combo);
-
-const old_per = ["phone","ipad","computer"];
-
-const new_per = [...old_per];
-
-new_per[1] = "sonyps";
-
-console.log(old_per);
-console.log(new_per);
-
-const arr3 = {
-    a: 1,
-    b: 2
-};
-
-const arr4 = {...arr3};
+     

d2c3d63c39b1c47be15cc5c81aa55c264a2e51c1
dilnuras <77263709+dilnuras@users.noreply.github.com>
2021-05-20 15:47:54 +0600

    urok 40

---
+++ JS/urok40/script.js
@@ -0,0 +1,32 @@
+"use strict";
+      
+    
+    
+//Динамическая типизация
+
+//console.log(String("text"));
+
+// let resinput = +prompt("Введите нмер страницы?","");
+
+//https://miukid.com/cat/
+
+
+//console.log(resinput +10);
+//console.log("https://miukid.com/cat/" + resinput); cтарый метод
+
+console.log(`https://miukid.com/cat/${resinput}`); //новый метод
+
+
+//
+function decimalToHexString(number)
+{
+  if (number < 0)
+  {
+    number = 0xFFFFFFFF + number + 1;
+  }
+
+  return number.toString(16).toUpperCase();
+}
+
+console.log(decimalToHexString(27));
+console.log(decimalToHexString(48.6));

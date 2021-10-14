6ed14986f99114e6e3c56f10973ed1884a1534b5
dilnuras <77263709+dilnuras@users.noreply.github.com>
2021-10-14 18:21:01 +0600

    zanovo

---
+++ React/68urok/src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _script = require("./script.js");
+
+// const AutoSlider= require('./script');
+var slider = new _script.AutoSlider(400, 200, 5, true);
+slider.play();
+slider.whoAmI();

84fd74282854fb53075588ae66fdb5765636f07a
dilnuras <77263709+dilnuras@users.noreply.github.com>
2021-10-14 18:15:07 +0600

    dopolnit

--- React/68urok/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _script = require("./script.js");
-
-// const AutoSlider= require('./script');
-var slider = new _script.AutoSlider(400, 200, 5, true);
-slider.play();
-slider.whoAmI();
